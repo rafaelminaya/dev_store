7aa3f13e6755bec2c87d0a089289793d156e0742
Rafael Minaya <rafael_mr20@outlook.com>
2023-04-04 20:57:42 -0500

    commit de instalacion

---
+++ src/test/java/com/rminaya/dev/store/DevStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rminaya.dev.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
